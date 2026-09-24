d10b6d8104ccaac9d97f503fe866b9a5d2ea5fe1
t-takahashi <t.takahashi@example.com>
2026-09-24 09:25:09 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    goToメソッドでトップページに遷移し、ページタイトルが一致しているかで遷移が成功しているか確認するテストコードを追加
    期待値：遷移したページのタイトルが「ログイン | LMS」であること
    
    fix:chromedriver.exe chomedriverの最新化
    
    ブラウザのアップデートに伴い、libフォルダ内のchromedriverを更新

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,7 +36,13 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+		//遷移成功チェック
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//スクリーンショット撮影
+		getEvidence(new Object() {
+		});
 	}
 
 }
