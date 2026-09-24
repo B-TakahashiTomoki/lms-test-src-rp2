36c57007faf191c65e0ba12483ade1e1ccb2abc9
t-takahashi <t.takahashi@example.com>
2026-09-24 10:47:21 +0900

    add:Case03.java 初回ログイン済みユーザーでの正常ログインテストコード追加
    初回ログイン済みの受講生ユーザーでログインし、正常にコース詳細画面に遷移するか確認するテストコードを追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,30 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+		//遷移成功チェック
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//スクリーンショット撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインID入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		//パスワード入力
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA011");
+		//ログインボタンクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		//コース詳細画面に遷移チェック
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		//スクリーンショット取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
