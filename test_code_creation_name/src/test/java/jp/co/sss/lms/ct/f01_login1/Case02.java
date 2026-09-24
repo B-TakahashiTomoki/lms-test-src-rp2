4d70f0921063d98431600fc28d0f63f0de32f0fa
t-takahashi <t.takahashi@example.com>
2026-09-24 09:48:43 +0900

    Add:Case02.java エラーメッセージ表示のテストコード追加
    DBに存在しないユーザーでログインし、エラーメッセージが表示されるか確認するテストコードを追加
    
    入力値
    ログインID:StudentAA001
    パスワード:StudentAAA01
    
    期待値
    エラーメッセージ「* ログインに失敗しました。」

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,30 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+		//遷移成功チェック
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//スクリーンショット撮影
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインID入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA001");
+		//パスワード入力
+		webDriver.findElement(By.id("password")).sendKeys("StudentAAA01");
+		//ログインボタンクリック
+		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		//エラーメッセージ比較
+		assertEquals("* ログインに失敗しました。", webDriver.findElement(By.cssSelector(".help-inline.error")).getText());
+		//スクリーンショット取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
